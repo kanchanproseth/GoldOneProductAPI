fa041cf6a628ad5447973306c04a9df36907e173
kanchanproseth <kanchanproseth@users.noreply.github.com>
2017-12-19 11:45:21 +0700

    add log in and register api

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
+from flask_pymongo import PyMongo
+from flask.ext.bcrypt import Bcrypt
 
 app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'API'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/API'
 app.secret_key = 'proseth123'
 api = Api(app)
-
+mongo = PyMongo(app)
+bcrypt = Bcrypt(app)
 
 class Product(Resource):
    
@@ -199,47 +205,84 @@ class Product(Resource):
 class Blog(Resource):
     def get(self):
         return {
-  "Blogs": [
-{
-"blog_title": "Gold One's 10th Anniversary Promotion",
-"posted_date": "03/12/2017",
-"Author": "Chung Uong",
-"short_desc": "In this special occasion, we are pleased to introduce a series of sale promotion, including a chance to win an iPhone X and a MSI GV62 7RC.",
-"image_uri": "desktop"
-},
-{
-"blog_title": "Gold One's 10th Anniversary Promotion",
-"posted_date": "03/12/2017",
-"Author": "Chung Uong",
-"short_desc": "In this special occasion, we are pleased to introduce a series of sale promotion, including a chance to win an iPhone X and a MSI GV62 7RC.",
-"image_uri": "desktop"
-},
-{
-"blog_title": "Gold One's 10th Anniversary Promotion",
-"posted_date": "03/12/2017",
-"Author": "Chung Uong",
-"short_desc": "In this special occasion, we are pleased to introduce a series of sale promotion, including a chance to win an iPhone X and a MSI GV62 7RC.",
-"image_uri": "desktop"
-}
-,{
-"blog_title": "Gold One's 10th Anniversary Promotion",
-"posted_date": "03/12/2017",
-"Author": "Chung Uong",
-"short_desc": "In this special occasion, we are pleased to introduce a series of sale promotion, including a chance to win an iPhone X and a MSI GV62 7RC.",
-"image_uri": "desktop"
-}
-,{
-"blog_title": "Gold One's 10th Anniversary Promotion",
-"posted_date": "03/12/2017",
-"Author": "Chung Uong",
-"short_desc": "In this special occasion, we are pleased to introduce a series of sale promotion, including a chance to win an iPhone X and a MSI GV62 7RC.",
-"image_uri": "desktop"
-}
-]
-}
+        "Blogs": [
+        {
+        "blog_title": "Gold One's 10th Anniversary Promotion",
+        "posted_date": "03/12/2017",
+        "Author": "Chung Uong",
+        "short_desc": "In this special occasion, we are pleased to introduce a series of sale promotion, including a chance to win an iPhone X and a MSI GV62 7RC.",
+        "image_uri": "desktop"
+        },
+        {
+        "blog_title": "Gold One's 10th Anniversary Promotion",
+        "posted_date": "03/12/2017",
+        "Author": "Chung Uong",
+        "short_desc": "In this special occasion, we are pleased to introduce a series of sale promotion, including a chance to win an iPhone X and a MSI GV62 7RC.",
+        "image_uri": "desktop"
+        },
+        {
+        "blog_title": "Gold One's 10th Anniversary Promotion",
+        "posted_date": "03/12/2017",
+        "Author": "Chung Uong",
+        "short_desc": "In this special occasion, we are pleased to introduce a series of sale promotion, including a chance to win an iPhone X and a MSI GV62 7RC.",
+        "image_uri": "desktop"
+        }
+        ,{
+        "blog_title": "Gold One's 10th Anniversary Promotion",
+        "posted_date": "03/12/2017",
+        "Author": "Chung Uong",
+        "short_desc": "In this special occasion, we are pleased to introduce a series of sale promotion, including a chance to win an iPhone X and a MSI GV62 7RC.",
+        "image_uri": "desktop"
+        }
+        ,{
+        "blog_title": "Gold One's 10th Anniversary Promotion",
+        "posted_date": "03/12/2017",
+        "Author": "Chung Uong",
+        "short_desc": "In this special occasion, we are pleased to introduce a series of sale promotion, including a chance to win an iPhone X and a MSI GV62 7RC.",
+        "image_uri": "desktop"
+        }
+        ]
+        }
 
 
-api.add_resource(Product, '/Product')
-api.add_resource(Blog, '/Blog')
+class UserRegister(Resource):
+    def post(self):
+        username = request.json['username']
+        password = request.json['password']
+        # print(pas)
+        user = mongo.db.users
+        pw_hash = bcrypt.generate_password_hash(password)
+        print(pw_hash)
+        for u in user.find():
+          if u['username'] == username:
+            return {'message': 'this user name is existed', 'code': '404'}
+        user_id = user.insert({'username': username, 'password': pw_hash})
+        new_user = user.find_one({'_id': user_id})
+        print(pw_hash)
+        return {'message': 'Register Success','code': '200'}, 200
 
-app.run(port = 5000, debug=True)
+
+class USerSignIn(Resource):
+    def post(self):
+        username = request.json['username']
+        password = request.json['password']
+        user = mongo.db.users
+        pw_hash = bcrypt.generate_password_hash(password)
+        u = user.find_one({'username': username})
+        print(u)
+        if user.find_one({'username': username}):
+            print(password)
+            print(u['password'])
+            print(u['username'])
+            if bcrypt.check_password_hash(u['password'], str(password)):
+                return {'message': 'Log In successfully', 'code': '200'}, 200
+            else:
+                return {'message': 'wrong password', 'code': '404'}
+        return {'message': 'please check your username again','code': '404'}
+
+if __name__ == '__main__':
+    api.add_resource(UserRegister, '/register')
+    api.add_resource(USerSignIn, '/signin')
+    api.add_resource(Product, '/Product')
+    api.add_resource(Blog, '/Blog')
+    app.run(port = 5000, debug=True)
